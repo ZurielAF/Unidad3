7747cb921ee58a6645a13e6630592973687cd6c3
ZurielAF <143724825+ZurielAF@users.noreply.github.com>
2023-09-28 08:36:05 -0600

    Zuriel Y Iker Andre

---
+++ Cajero.java
@@ -0,0 +1,61 @@
+
+package cajero;
+import java.util.Scanner;
+
+public class Cajero {
+ 
+    public static void main(String[] args) {
+ 
+   int saldo = 3000, retirar, deposito;
+        Scanner scanner = new Scanner(System.in);
+        
+        {
+            System.out.println("BIENVENIDO AL CAJERO AUTOMATICO\n ");
+            System.out.println("-----------------------------------");
+            System.out.println("PRESIONE 1 PARA RETIRAR SALDO");
+            System.out.println("PRESIONE 2 PARA DEPOSITAR SALDO");
+            System.out.println("PRESIONE 3 PARA VERIFICAR SU SALDO");
+            System.out.println("------------------------------------\n");
+            System.out.print("SELECCIONE UNA OPCION:");
+            int n = scanner.nextInt();
+            switch(n)
+            {
+                case 1:
+                System.out.print("\nINGRESE LA CANTIDAD QUE DESEA RETIRAR:");
+                retirar = scanner.nextInt();
+                if(saldo >= retirar)
+                {
+                    saldo = saldo - retirar;
+                    System.out.println("\nDINERO RETIRADO EXITOSAMENTE");
+                }
+                else
+                {
+                    System.out.println("\nSALDO INSUFICIENTE");
+                }
+ 
+                System.out.println("");
+                break;
+ 
+                case 2:
+                System.out.print("\nINGRESE LA CANTIDAD QUE DESEA DEPOSITAR:");
+                deposito = scanner.nextInt();
+                saldo = saldo + deposito;
+                System.out.println("\nSU DINERO HA SIDO DEPOSITADO EXITOSAMENTE");
+ 
+                break;
+ 
+                case 3:
+                System.out.println("SALDO : "+saldo);
+ 
+                break;
+ 
+                case 4:
+                System.out.println("OPERACION INVALIDA, POR FAVOR SELECCIONE OTRA OPCION:");
+                System.exit(0);
+            }
+        }
+    }
+}
+    
+    
+
